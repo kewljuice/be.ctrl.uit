dc6aa567cbf358cb47c0686817c8a1fa3ba593dc
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2018-07-19 16:03:25 +0200

    refs #entity added schema and updater.

--- uit.php
+++ uit.php
@@ -139,31 +139,3 @@ function uit_civicrm_angularModules(&$angularModules) {
 function uit_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   _uit_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
-
-// --- Functions below this ship commented out. Uncomment as required. ---
-
-/**
- * Implements hook_civicrm_preProcess().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
- *
- * function uit_civicrm_preProcess($formName, &$form) {
- *
- * } // */
-
-/**
- * Implements hook_civicrm_navigationMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
- *
- * function uit_civicrm_navigationMenu(&$menu) {
- * _uit_civix_insert_navigation_menu($menu, NULL, array(
- * 'label' => E::ts('The Page'),
- * 'name' => 'the_page',
- * 'url' => 'civicrm/the-page',
- * 'permission' => 'access CiviReport,access CiviContribute',
- * 'operator' => 'OR',
- * 'separator' => 0,
- * ));
- * _uit_civix_navigationMenu($menu);
- * } // */
